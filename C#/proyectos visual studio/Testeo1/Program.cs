9fb33a59b04c08f0da3cf18525cbc844099429b2
Aitor Marin <amarin@ifr.es>
2022-10-24 17:32:59 +0200

    AutoMouse Created - Working on AutoKeys

---
+++ C#/proyectos visual studio/Testeo1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Runtime.InteropServices;
+using System.Security.Principal;
+using System.Windows;
+
+namespace Testeo1
+{
+    public class Program 
+    {
+        static void Main(string[] args)
+        {
+            //AutoMouse autoMouse = new AutoMouse(50, 60);
+            //AutoMouse.Execute();
+            //Pause();
+            [DllImport("User32.dll")]
+            static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+            [DllImport("User32.dll")]
+            static extern int SetForegroundWindow(IntPtr hWnd);
+
+
+            IntPtr ptrFF = FindWindow(null,"Microsoft Teams");
+            SetForegroundWindow(ptrFF);
+            
+
+            Console.WriteLine(""+ConsoleKey.D + ConsoleKey.S + ConsoleKey.A + ConsoleKey.W);
+
+            if (Console.ReadKey().Equals(ConsoleKey.LeftWindows))
+            {
+                Console.WriteLine("Tecla windows izquierda pulsada");
+            }
+
+            if (Console.ReadKey().Equals(ConsoleKey.RightWindows))
+            {
+                Console.WriteLine("Tecla windows derecha pulsada");
+            }
+            Pause();
+            
+        }
+        public static void Pause()
+        {
+            do { Console.WriteLine("\n\n\n\n\n\n\t\t\t\t\tPress SpaceBar to Exit"); } while (Console.ReadKey(true).Key != ConsoleKey.Spacebar);
+        }
+
+    }
+}

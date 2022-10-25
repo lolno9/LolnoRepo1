fafd1c10136139ee8ad052922b03decbc6ca35e5
Aitor Marin <amarin@ifr.es>
2022-10-25 09:51:01 +0200

    AutoKey working

--- C#/proyectos visual studio/Testeo1/Program.cs
+++ C#/proyectos visual studio/Testeo1/Program.cs
@@ -1,6 +1,6 @@
-﻿using System.Runtime.InteropServices;
-using System.Security.Principal;
-using System.Windows;
+﻿using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
 
 namespace Testeo1
 {
@@ -8,37 +8,44 @@ namespace Testeo1
     {
         static void Main(string[] args)
         {
+            ///////////////////////////////////////////////////
             //AutoMouse autoMouse = new AutoMouse(50, 60);
             //AutoMouse.Execute();
             //Pause();
+            ///////////////////////////////////////////////////
             [DllImport("User32.dll")]
             static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
             [DllImport("User32.dll")]
             static extern int SetForegroundWindow(IntPtr hWnd);
-
-
-            IntPtr ptrFF = FindWindow(null,"Microsoft Teams");
-            SetForegroundWindow(ptrFF);
-            
-
-            Console.WriteLine(""+ConsoleKey.D + ConsoleKey.S + ConsoleKey.A + ConsoleKey.W);
-
-            if (Console.ReadKey().Equals(ConsoleKey.LeftWindows))
+            Process[] p = Process.GetProcesses();
+            int cont = 0;
+            Process toWork;
+            foreach (Process proc in p)
             {
-                Console.WriteLine("Tecla windows izquierda pulsada");
-            }
-
-            if (Console.ReadKey().Equals(ConsoleKey.RightWindows))
-            {
-                Console.WriteLine("Tecla windows derecha pulsada");
+                if (proc.ProcessName.Contains("eams"))
+                {
+                    Console.WriteLine("Process found: " + proc.ProcessName);
+                    Console.WriteLine("Machine Name: " + proc.MachineName);
+                    Console.WriteLine("Main Window Title: " + proc.MainWindowTitle);
+                    Console.WriteLine("Process Id " + proc.Id);
+                    if (proc.MainWindowTitle.Contains("Consultores"))
+                    {
+                        cont = proc.Id;
+                        Console.WriteLine("ID get");
+                        break;
+                    }
+                }
             }
             Pause();
-            
+            toWork = Process.GetProcessById(cont);
+            IntPtr ptrFF = toWork.MainWindowHandle;
+            SetForegroundWindow(ptrFF);
+            SendKeys.SendWait("" + ConsoleKey.D+":"+((byte)ConsoleKey.D) + ConsoleKey.S + ":" + ((byte)ConsoleKey.S) + ConsoleKey.A + ":" + ((byte)ConsoleKey.A) + ConsoleKey.W + ":" + ((byte)ConsoleKey.W) + "From Console"+"\n");
+            Pause();
         }
         public static void Pause()
         {
             do { Console.WriteLine("\n\n\n\n\n\n\t\t\t\t\tPress SpaceBar to Exit"); } while (Console.ReadKey(true).Key != ConsoleKey.Spacebar);
         }
-
     }
 }

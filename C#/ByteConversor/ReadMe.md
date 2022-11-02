06e0c38f15ef5c1cf8710dcd532cab1718b7cbe9
Aitor Marin <amarin@ifr.es>
2022-11-02 11:34:45 +0100

    ByteConversor updated to V0.0.2

---
+++ C#/ByteConversor/ReadMe.md
@@ -0,0 +1,10 @@
+#V0.0.1
+Read all bytes from a file, convert those bytes to 8-padded bits and create a new file with
+path&filename
+bytes concatenated with ';' Ex: 105;110;100;101;
+8-padded bits concatenated without separator Ex: 01101001011011100110010001100101
+
+#V0.0.2
+UPDATE
+Show line with progress
+Erased las ';' from bytes, now the line ends with "\n"
--- C#/proyectos visual studio/ByteConversor/Program.cs
+++ C#/proyectos visual studio/ByteConversor/Program.cs
@@ -28,57 +28,94 @@ namespace ByteConversor
             Console.WriteLine(String.IsNullOrWhiteSpace(testList[1]) ? testIn[1] : testList[1]);//If custom input path empty, set to default
             Console.WriteLine(testList[0]);
             Console.WriteLine("\n\n");
+            //Check if values inside list are empty strings or not
             if(testList.Count == 2)
             {
-                if (string.IsNullOrWhiteSpace(testList[0]))
+                if (string.IsNullOrWhiteSpace(testList[0]))//If empty put default values
                 {
                     testList.Remove(testList[0]);
                     testList.Insert(0, testIn[0]);
 
                 }
-                if (string.IsNullOrWhiteSpace(testList[1]))
+                if (string.IsNullOrWhiteSpace(testList[1]))//If empty put default values
                 {
                     testList.Remove(testList[1]);
                     testList.Insert(1, testIn[1]);
                 }
             }
+            //Create BitConversor object with path and file name
             BitConversor bitConversor = new BitConversor(testOut[0], testOut[1]);
-            bitConversor.SetFullPATH();
-            List<byte> rawData = new List<byte>();
-            List<string> byteData = new List<string>();
-            string output = testList[1] + testList[0] + "\n";
+            bitConversor.SetFullPATH();//Concatenate the path in one string
+            //Declare the lists thats gona be using
+            List<byte> rawData = new List<byte>();//Read data from fle
+            List<string> byteData = new List<string>();//Processed data ready to put in a new/existing file
+
+            string output = testList[1] + testList[0] + "\n";//String used to concatenate data and put inside the new file
+
+            Console.WriteLine("Reading bytes of: " + testList[1] + testList[0]);
+
             if (File.Exists(testList[1] + testList[0]))
             {
-                rawData = File.ReadAllBytes(testList[1] + testList[0]).ToList();
+                rawData = File.ReadAllBytes(testList[1] + testList[0]).ToList();//Read bytes of file and put it in rawData List
                 Console.WriteLine("File: " + testList[1] + testList[0] + " Opened");
+                int contador = 1;
                 foreach (byte b in rawData)
                 { //Valorate if use ';' separator on bytes
                     //Console.Write(b);
-                    byteData.Add(bitConversor.Get8PBinFromByte(b));
-                    output += b + ";";
-                    
+                    byteData.Add(bitConversor.Get8PBinFromByte(b));//Pass each individual byte to bit and put it into byteData List
+                    if(contador == rawData.Count())
+                    {
+                        output += b;
+                    } else
+                    {
+                        output += b + ";";
+                    }
+                     //Concatenate all bytes from rawData folowed by ';' in the output String
+                    //////////////////////////////////////////////////////
+                    ///This loop works for refreshing the progress line
+                    string consoleOut = contador + "/" + rawData.Count();
+                    Console.Write(consoleOut);
+                    foreach(char c in consoleOut)
+                    {
+                        Console.Write("\b");
+                    }
+                    //////////////////////////////////////////////////////
+                    contador++;                    
                 }
+
                 Console.WriteLine("\n");
-                output += "\n";
-                foreach (string s in byteData)
-                {//No need of ';' separator here cause bits always 8-Padded
+                output += "\n";//Make new line in output String
+                Console.WriteLine("Processing bytes of: " + testList[1] + testList[0]);
+                contador = 1;
+                foreach (string s in byteData) 
+                {
                     //Console.Write(s);
-                    output += s;
+                    output += s; //Concatenate all bits in byteData (As they are allways 8-padded, no need to put separator here)
+                    //////////////////////////////////////////////////////
+                    ///This loop works for refreshing the progress line
+                    string consoleOut = contador + "/" + byteData.Count();
+                    Console.Write(consoleOut);
+                    foreach (char c in consoleOut)
+                    {
+                        Console.Write("\b");
+                    }
+                    //////////////////////////////////////////////////////
+                    contador++;
                 }
-                if (File.Exists(bitConversor.PATHGS))
-                {
-                    File.AppendAllText(bitConversor.PATHGS, "\n\n" + output);
-                    Console.WriteLine("\nFile " + bitConversor.PATHGS + " Writed Succesfully");
-                }
-                if (!File.Exists(bitConversor.PATHGS))
-                {
-                    File.WriteAllText(bitConversor.PATHGS, output);
-                    Console.WriteLine("\nFile "+ bitConversor.PATHGS + " Created Succesfully");
-                }
-                //Console.WriteLine("\n" + bitConversor.PATHGS);
             }
-            Pause();   
-        }
+            if (File.Exists(bitConversor.PATHGS))
+            {
+                File.AppendAllText(bitConversor.PATHGS, "\n\n" + output);
+                Console.WriteLine("\nFile " + bitConversor.PATHGS + " Writed Succesfully");
+            }
+            if (!File.Exists(bitConversor.PATHGS))
+            {
+                File.WriteAllText(bitConversor.PATHGS, output);
+                Console.WriteLine("\nFile "+ bitConversor.PATHGS + " Created Succesfully");
+            }
+            //Console.WriteLine("\n" + bitConversor.PATHGS);
+            Pause(); 
+        }             
         public static void Pause()
         {
             do { Console.WriteLine("\n\n\n\n\n\n\t\t\t\t\tPress SpaceBar to Exit"); } while (Console.ReadKey(true).Key != ConsoleKey.Spacebar);
